944dfc295bc9d5247c0fbdc7ccff2924a7e47427
pufcjoe <joepp1658@gmail.com>
2026-06-30 16:54:06 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const ROUND_LABEL = { W: "Champion", F: "Finalist", SF: "Semifinalist", QF: "Qua
 
 const ALIASES = {
   vitzaru: "vitzaru", vitzius: "vitzaru", joethepayne: "vitzaru", kunsaz: "vitzaru",
-  kuzuderee: "kuzuderee", MAJOR1KLASY: "kuzuderee",
+  major1klasy: "kuzuderee",
   vitzonlyjoshv: "vitzaru", vitzari: "vitzaru",
   flenxu: "flenxuu", ilyanasheii: "ilyanashei", xvxcai: "xvcxai", cristyvalentin: "cristy_valentin",
   khaledraii: "wingstopeffect", beardofglaz3: "wingstopeffect",
